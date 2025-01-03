81bd1e19b079ba29b03f50a4009781d9692d28f5
ktny <katsunori.t10@gmail.com>
2025-01-03 23:12:46 +0900

    Update Dockerfile and entrypoint for uwsgi support

--- django-benchmark/src/entrypoint.sh
+++ django-benchmark/src/entrypoint.sh
@@ -17,4 +17,7 @@ python manage.py migrate
 # gunicorn --bind 0.0.0.0:8000 -k gthread --threads 17 common.wsgi:application
 
 # マルチプロセス・マルチスレッド
-gunicorn --bind 0.0.0.0:8000 -k gthread --workers 17 --threads 17 common.wsgi:application
+# gunicorn --bind 0.0.0.0:8000 -k gthread --workers 17 --threads 17 common.wsgi:application
+
+# uwsgi
+uwsgi --http 0.0.0.0:8000 --processes=17 --threads=17 --module common.wsgi:application
